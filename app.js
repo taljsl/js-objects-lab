684c2c1354eba389447e6660b11e0594129675de
Josh L <joshua.levine42@gmail.com>
2024-10-19 16:53:04 -0400

    first 9 done

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ for (let i = 0; i < pokemon.length; i++) {
     count3++
   }
 }
-console.log(game.party);
+// console.log(game.party);
 
 
 
@@ -117,5 +117,51 @@ Solve Exercise 6 here:
 for (let i = 0; i < game.gyms.length; i++) {
   if (game.gyms[i].difficulty < 3) {
     game.gyms[i].completed = true
+  }
+}
+/*
+Exercise 7
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. When working with an array of objects, the splice() array method is ideal for replacing one element with another. 
+
+
+Solve Exercise 7 here:
+*/
+
+game.party.splice(0,1, pokemon[7]) // go into game then party, remove the element at index zero and replace with 1 item at index 7 of pokemon
+
+/*
+Exercise 8
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 8 here:
+*/
+
+for (let i = 0; i < game.party.length; i++) {
+  console.log(game.party[i].name)
+}
+
+/*
+Exercise 9
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
+
+
+Solve Exercise 9 here:
+*/
+
+
+for (let i = 0; i < pokemon.length; i++) {
+  if(pokemon[i].starter === true) {
+    console.log(pokemon[i])
   }
 }
